5a27f7b8a42a1f626604cfd74f8ba1e2cf585df7
Gibson Nakokonya <Gibson.Nakokonya@equitybank.co.ke>
2026-08-13 21:53:41 +0300

    can now autocomplete within the literal strings

--- src/main/kotlin/com/silali/angularI18n/HtmlTranslationKeyCompletionContributor.kt
+++ src/main/kotlin/com/silali/angularI18n/HtmlTranslationKeyCompletionContributor.kt
@@ -9,39 +9,47 @@ import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.lang.javascript.JSTokenTypes
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
 import org.angular2.lang.expr.psi.Angular2Interpolation
-
 
 class HtmlTranslationKeyCompletionContributor : CompletionContributor() {
 
     init {
-
         extend(
             CompletionType.BASIC,
-            PlatformPatterns.psiElement(JSTokenTypes.STRING_LITERAL)
-                .inside(Angular2Interpolation::class.java),
+            PlatformPatterns.psiElement(),
             object : CompletionProvider<CompletionParameters>() {
+
                 override fun addCompletions(
                     parameters: CompletionParameters,
                     context: ProcessingContext,
                     result: CompletionResultSet
                 ) {
-                    LOG.info("TranslationKeyCompletionContributor fired at offset ${parameters.offset}")
+                    val position = parameters.position
+
+                    val isStringLiteral = position.node?.elementType == JSTokenTypes.STRING_LITERAL
+                    val insideInterpolation =
+                        PsiTreeUtil.getParentOfType(position, Angular2Interpolation::class.java) != null
+
+                    LOG.info(
+                        "FIRED offset=${parameters.offset} elementType=${position.node?.elementType} " +
+                                "isStringLiteral=$isStringLiteral insideInterpolation=$insideInterpolation"
+                    )
+
+                    if (!isStringLiteral || !insideInterpolation) return
 
                     FAKE_TRANSLATION_KEYS.forEach { key ->
-                        result.addElement(
-                            LookupElementBuilder.create(key)
-                                .withTypeText("i18n key")
-                        )
+                        result.addElement(LookupElementBuilder.create(key))
                     }
                 }
-            },
+            }
         )
     }
 
     companion object {
-        private val LOG = Logger.getInstance(HtmlTranslationKeyCompletionContributor::class.java)
+        private val LOG =
+            Logger.getInstance(HtmlTranslationKeyCompletionContributor::class.java)
 
         private val FAKE_TRANSLATION_KEYS = listOf(
             "AUTH.SIGN-IN",
